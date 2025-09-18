798294521ed454805ceaa2f58343d2859dae69cb
Nicole <nicolegrecchibraz@gmail.com>
2025-09-18 10:53:43 -0500

    primeiro teste automatizado

---
+++ cypress/e2e/loja-ebac-3/login.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+
+describe("funcionalidade login",()=> {
+
+    it("deve fazer login com sucesso" , () => {
+        cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/")
+        cy.get('#username').type("nicoleteste@com.br")
+        cy.get('#password').type("teste@123")
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should("contain" , "Olá, nicoleteste (não é nicoleteste? Sair)")
+        
+      })
+})
