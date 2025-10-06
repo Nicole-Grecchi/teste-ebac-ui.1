3744e3a0cf44a319080c223215263704282c8a88
Nicole <nicolegrecchibraz@gmail.com>
2025-10-06 11:00:54 -0500

    pege Objects

--- cypress/e2e/loja-ebac-3/produtos.cy.js
+++ cypress/e2e/loja-ebac-3/produtos.cy.js
@@ -1,19 +1,29 @@
 /// <reference types="cypress"/>
 
+import produtosPage from "../../support/page-objects/produtos.page";
+
 describe('funcionalidade: produtos', () => {
 
     beforeEach(() => {
-        cy.visit("produtos")
+       produtosPage.visitarUrl()
     });
     it('deve selecionar um produto da lista', () => {
-        cy.get(' .product-block ')
-           //.first()
-         //.last()
-          .contains("Arcadio Gym Short")
-          .click()
-          
+        produtosPage.buscarProdutosLista("Argus All-Weather Tank")
           cy.get('#tab-title-description > a').should("contain" , "Descrição")
         
     });
     
+    it.only('deve buscar um produto com sucesso', () => {
+        produtosPage.buscarProdutos('Ingrid Running Jacket')
+        cy.get('.product_title').should('contain' , 'Ingrid Running Jacket')
+    });
+
+    it('deve visitar a pagina do produto', () => {
+        
+    });
+
+    it('deve adicionar produto ao carrinho ', () => {
+        
+    });
+
 });
