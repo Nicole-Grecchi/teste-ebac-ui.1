0aae78ac162f08f4f13d6aa0d74ffba2c0dd8553
Nicole <nicolegrecchibraz@gmail.com>
2025-10-13 11:29:01 -0500

    massa de codigos

--- cypress/e2e/loja-ebac-3/produtos.cy.js
+++ cypress/e2e/loja-ebac-3/produtos.cy.js
@@ -13,16 +13,34 @@ describe('funcionalidade: produtos', () => {
         
     });
     
-    it.only('deve buscar um produto com sucesso', () => {
+    it('deve buscar um produto com sucesso', () => {
         produtosPage.buscarProdutos('Ingrid Running Jacket')
         cy.get('.product_title').should('contain' , 'Ingrid Running Jacket')
     });
 
     it('deve visitar a pagina do produto', () => {
-        
+        produtosPage.visitarProdutos('Zeppelin Yoga Pant')
+         cy.get('.product_title').should('contain' , 'Zeppelin Yoga Pant')
     });
 
     it('deve adicionar produto ao carrinho ', () => {
+      let qtd = 7 
+      produtosPage.buscarProdutos('Augusta Pullover Jacket')
+      produtosPage.addProdutosCarrinho('XS', 'Blue', qtd)
+      cy.get('.woocommerce-message').should("contain" ,   " “Augusta Pullover Jacket” foram adicionados no seu carrinho.")
+
+        
+    });
+
+   
+    it.only('deve adicionar produto ao carrinho buscando da massa de dados ', () => {
+      cy.fixture('produtos').then(dados => {
+      produtosPage.buscarProdutos(dados[1].nomeProduto)
+      produtosPage.addProdutosCarrinho(dados[1].tamanho,dados[1].cor,dados[1].quantidade)
+      cy.get('.woocommerce-message').should("contain" , dados[1].nomeProduto  )
+
+      })
+      
         
     });
 
--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -3,11 +3,13 @@ class ProdutosPage{
    visitarUrl() {
      cy.visit("produtos")
    }
+    
 
-   buscarProdutos(nomeProduto) {
-      cy.get('[name="s"]').eq(1).type(nomeProduto)
-      cy.get('.button-search').eq(1).click()
-   }
+  buscarProdutos(nomeProduto){
+   
+   cy.get('[name="color"]').eq(1).click().type(nomeProduto)
+   cy.get('.button-search').eq(1).click()
+  }
 
    buscarProdutosLista(nomeProduto) {
         cy.get(' .products > .row')
@@ -16,11 +18,19 @@ class ProdutosPage{
           
    }
 
-   visitarProdutos() {
+   visitarProdutos(nomeProduto) {
+      // cy.visit(`produtos/${nomeProduto}`)
+       const urlFormatada =  nomeProduto.replace(/ /g, '-' )
+       cy.visit(`produtos/${urlFormatada}`)
+
 
    }
 
-   addProdutosCarrinho() {
+   addProdutosCarrinho(tamanho, cor, quantidade) {
+      cy.get('.button-variable-item-' + tamanho ).click()
+      cy.get(`.button-variable-item-${cor}`).click()
+      cy.get('.input-text').clear().type(quantidade)
+      cy.get('.single_add_to_cart_button').click()
 
    }
 
