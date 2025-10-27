2473044d8f43827042477b71938edbf0db81e6d4
Nicole <nicolegrecchibraz@gmail.com>
2025-10-27 11:47:50 -0500

    mudança gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'c8oqaw',
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
     defaultCommandTimeout: 15000,
     viewportWidth: 1000,
     viewportHeight: 660
